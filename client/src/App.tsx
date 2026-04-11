7247e0794b59d989b8fa81a33b47c4371442f4ba
nextwaveaisuite <nextwaveaisuite@gmail.com>
2026-04-11 22:18:20 +1000

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,9 @@ import Leads from "./pages/Leads";
 import Emails from "./pages/Emails";
 import CRM from "./pages/CRM";
 import PageBuilder from "./pages/PageBuilder";
+import ClientLogin from "./pages/ClientLogin";
+import Pricing from "./pages/Pricing";
+import ClientPortal from "./pages/ClientPortal";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
@@ -16,6 +19,9 @@ export default function App() {
       <Route path="/email-approval" component={Emails} />
       <Route path="/crm" component={CRM} />
       <Route path="/page-builder" component={PageBuilder} />
+      <Route path="/client-login" component={ClientLogin} />
+      <Route path="/pricing" component={Pricing} />
+      <Route path="/portal" component={ClientPortal} />
       <Route component={NotFound} />
     </Switch>
   );
